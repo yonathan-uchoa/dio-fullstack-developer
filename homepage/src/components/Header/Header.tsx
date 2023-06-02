d08bf6de45e356ff9f5e7514fcd194bf5114d1cb
Yonathan Uchôa Simão Kaveski <yonathan.uchoa@gmail.com>
2023-06-02 00:11:22 -0300

    feat: add conditional header from log-in and log-out

--- homepage/src/components/Header/Header.tsx
+++ homepage/src/components/Header/Header.tsx
@@ -1,9 +1,46 @@
+import { Button } from '@chakra-ui/react'
 import './Header.css'
+import { useNavigate } from 'react-router-dom' 
+import {changeLocalStorage} from '../../services/storage'
+import { useContext } from 'react'
+import { AppContext } from '../AppContext'
 
-export const Header  = () => {
+
+const LogginHeader = (logout: () => void) => {  
   return(
+    <nav className='header' >
+        Dio Bank
+        <Button
+          onClick={logout}
+          colorScheme="teal"
+          size={'sm'}
+          float='right'
+        >
+          Log-out!
+        </Button>
+    </nav>
+  )
+}
+
+const DefaultHeader = () => {
+  return (
     <div className='header'>
       Dio Bank
     </div>
   )
+}
+
+export const Header = () => {
+  const {isLoggedIn} = useContext(AppContext)
+  const navigate = useNavigate()
+  const { setItemState } = useContext(AppContext)
+
+  const logout = () => {
+    setItemState('isLoggedIn', false)
+    changeLocalStorage('isLoggedIn', 'false')
+    navigate('/')
+  }
+  if(isLoggedIn)
+    return LogginHeader(logout)
+  return DefaultHeader()
 }
